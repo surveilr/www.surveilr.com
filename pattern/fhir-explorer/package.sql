6323fd2e3c1fccea9122f85a4fd7c4b53bbbf283
shah <shah@users.noreply.github.com>
2024-09-24 18:42:40 +0000

    deploy: 1dfc9cecb454ab6c174626c61cfe13f51cd46ab2

--- pattern/fhir-explorer/package.sql
+++ pattern/fhir-explorer/package.sql
@@ -1,5 +1,2 @@
 Error generate SQL for fhir-explorer from `deno run -A ./lib/pattern/fhir-explorer/package.sql.ts` in /home/runner/work/www.surveilr.com/www.surveilr.com: Command failed: deno run -A ./lib/pattern/fhir-explorer/package.sql.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/whitespace.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts
 [0m[1m[31merror[0m: Expected {, got interface at https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/docs/tbls.ts:36:7
