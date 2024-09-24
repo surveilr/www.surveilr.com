de9ab4b4221988ebb98cb251217b382e37e68e34
resulraveendran <resulraveendran@users.noreply.github.com>
2024-09-24 11:51:31 +0000

    deploy: 31669759ba1884c481e7c06335f4c717dba637c2

--- pattern/fhir-explorer/package.sql
+++ pattern/fhir-explorer/package.sql
@@ -1,7 +1,2 @@
 Error generate SQL for fhir-explorer from `deno run -A ./lib/pattern/fhir-explorer/package.sql.ts` in /home/runner/work/www.surveilr.com/www.surveilr.com: Command failed: deno run -A ./lib/pattern/fhir-explorer/package.sql.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/os-user.ts
-[0m[32mDownload[0m https://deno.land/std@0.224.0/path/mod.ts
-[0m[32mDownload[0m https://deno.land/std@0.224.0/ulid/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts
-[0m[1m[31merror[0m: Expected {, got interface at https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/docs/tbls.ts:36:7
+[0m[1m[31merror[0m: Expected {, got Callables at https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts:40:12
--- pattern/std/package.sql
+++ pattern/std/package.sql
@@ -1,18 +1,14 @@
 Error generate SQL for std from `deno run -A ./lib/std/package.sql.ts` in /home/runner/work/www.surveilr.com/www.surveilr.com: Command failed: deno run -A ./lib/std/package.sql.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts
-[0m[32mDownload[0m https://deno.land/std@0.224.0/path/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/mod.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/whitespace.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/mod.ts
-[0m[32mDownload[0m https://deno.land/std@0.224.0/ulid/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/os-user.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/mod.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/docs/tbls.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/enum-table.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/typical.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/typical-cli.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/diagram/plantuml-ie-notation.ts
+[0m[32mDownload[0m https://deno.land/std@0.224.0/path/mod.ts
+[0m[32mDownload[0m https://deno.land/std@0.224.0/ulid/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/os-user.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/mod.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/info-model.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/diagram/mod.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/rust/mod.ts
-[0m[1m[31merror[0m: Expected {, got interface at https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/docs/tbls.ts:36:7
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/diagram/mod.ts
+[0m[1m[31merror[0m: Expected {, got Callables at https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts:40:12
