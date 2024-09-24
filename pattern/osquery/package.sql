5ebc267e104bd19cf8aaf860c391418a6ed08eec
resulraveendran <resulraveendran@users.noreply.github.com>
2024-09-24 11:02:15 +0000

    deploy: 31669759ba1884c481e7c06335f4c717dba637c2

--- pattern/osquery/package.sql
+++ pattern/osquery/package.sql
@@ -1,2 +1,5 @@
 Error generate SQL for osquery from `deno run -A ./lib/pattern/osquery/package.sql.ts` in /home/runner/work/www.surveilr.com/www.surveilr.com: Command failed: deno run -A ./lib/pattern/osquery/package.sql.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/mod.ts
+[0m[32mDownload[0m https://deno.land/std@0.224.0/path/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/os-user.ts
 [0m[1m[31merror[0m: Expected {, got interface at https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/docs/tbls.ts:36:7
