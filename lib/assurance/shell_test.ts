7076b7202adc82a6dbc5b17db95128f52b7ac9da
Baasit <baasisek01@gmail.com>
2024-09-25 11:56:27 +0100

    chore(app): moved assurance from rsc to surveilr

---
+++ lib/assurance/shell_test.ts
@@ -0,0 +1,3 @@
+//TODO:: surveilr shell < ./study-files-rsc-scripts/dd.sql
+//TODO: cat ./study-files-rsc-scripts/dd.sql | just run shell
+// TODO: use surveilr shell ./study-files-rsc-scripts/vv.sql --cmd "select * from session_state_ephemeral" --import-env "CARGO*" to test importing envs
