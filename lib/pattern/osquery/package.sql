59494fe66530984d6707426c9627eee20e8d0e04
nidhin077 <nidhin077@users.noreply.github.com>
2024-09-25 04:34:50 +0000

    deploy: 2adf7fadea055b70a979ac6c60b0915477d9ab1a

--- lib/pattern/osquery/package.sql
+++ lib/pattern/osquery/package.sql
@@ -1,17 +1,24 @@
 Error executing pattern/osquery/package.sql: Command failed: deno run -A lib/pattern/osquery/package.sql.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/os-user.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/mod.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/docs/tbls.ts
-[0m[32mDownload[0m https://deno.land/std@0.224.0/ulid/mod.ts
-[0m[32mDownload[0m https://deno.land/std@0.224.0/path/mod.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/whitespace.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/dql/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/graph.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/dml/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/emit/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/ddl/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts
+[0m[32mDownload[0m https://deno.land/std@0.224.0/path/mod.ts
+[0m[32mDownload[0m https://deno.land/std@0.224.0/ulid/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/universal/os-user.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/typical.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/enum-table.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/diagram/plantuml-ie-notation.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/pattern/typical/typical-cli.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/diagram/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/info-model.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/polygenix/rust/mod.ts
 [0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/domain/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/ddl/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/emit/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/graph.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/dql/mod.ts
+[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/render/dml/mod.ts
 [0m[1m[31merror[0m: Expected {, got Callables at https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.14.8/lib/reflect/callable.ts:40:12
