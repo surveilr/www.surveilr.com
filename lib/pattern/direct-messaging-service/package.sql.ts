0287f8b9391c935566de7aa7b5068129691586ae
Ditty K.M <ditty@citrusinformatics.com>
2024-09-26 14:45:32 +0530

    feat: add direct messaging service #5

---
+++ lib/pattern/direct-messaging-service/package.sql.ts
@@ -0,0 +1,437 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-run --allow-sys
+import { sqlPageNB as spn } from "./deps.ts";
+import {
+  console as c,
+  orchestration as orch,
+  shell as sh,
+  uniformResource as ur,
+} from "../../std/web-ui-content/mod.ts";
+
+/**
+ * These pages depend on ../../std/package.sql.ts being loaded into RSSD (for nav).
+ *
+ * TODO: in TypicalSqlPageNotebook.SQL() call at the bottom of this code it
+ * probably makese sense to just add all dependent notebooks into the sources
+ * list like so:
+ *
+ *   TypicalSqlPageNotebook.SQL(new X(), new Y(), ..., new FhirSqlPages())
+ *
+ * where X, Y, etc. are in lib/std/content/mod.ts
+ */
+
+// custom decorator that makes navigation for this notebook type-safe
+function dmsNav(route: Omit<spn.RouteConfig, "path" | "parentPath">) {
+  return spn.navigationPrime({
+    ...route,
+    parentPath: "/dms",
+  });
+}
+
+/**
+ * These pages depend on ../../prime/ux.sql.ts being loaded into RSSD (for nav).
+ */
+export class DirectMessageSqlPages extends spn.TypicalSqlPageNotebook {
+  // TypicalSqlPageNotebook.SQL injects any method that ends with `DQL`, `DML`,
+  // or `DDL` as general SQL before doing any upserts into sqlpage_files.
+  navigationDML() {
+    return this.SQL`
+      -- delete all /dms-related entries and recreate them in case routes are changed
+      DELETE FROM sqlpage_aide_navigation WHERE path like '/dms%';
+      ${this.upsertNavSQL(...Array.from(this.navigation.values()))}
+    `;
+  }
+
+  @spn.navigationPrimeTopLevel({
+    caption: "Direct Protocol Email System",
+    description: "Email system with direct protocol",
+  })
+  "dms/index.sql"() {
+    return this.SQL`
+      WITH navigation_cte AS (
+          SELECT COALESCE(title, caption) as title, description
+            FROM sqlpage_aide_navigation
+           WHERE namespace = 'prime' AND path = '/dms'
+      )
+      SELECT 'list' AS component, title, description
+        FROM navigation_cte;
+      SELECT caption as title, COALESCE(url, path) as link, description
+        FROM sqlpage_aide_navigation
+       WHERE namespace = 'prime' AND parent_path = '/dms'
+       ORDER BY sibling_order;`;
+  }
+  @dmsNav({
+    caption: "Inbox",
+    description: ``,
+    siblingOrder: 1,
+  })
+  "dms/inbox.sql"() {
+    return this.SQL`
+      ${this.activePageTitle()}
+
+      SELECT 'table' AS component,
+            'subject' AS markdown,
+            'Column Count' as align_right,
+            TRUE as sort,
+            TRUE as search;
+
+      SELECT id,
+      "from",
+       '[' || subject || '](/dms/email-detail.sql?id=' || id || ')' AS "subject",
+      date
+      from inbox
+      `;
+  }
+  @spn.shell({ breadcrumbsFromNavStmts: "no" })
+  "dms/email-detail.sql"() {
+    return this.SQL`
+
+    select
+    'breadcrumb' as component;
+    select
+        'Home' as title,
+        '/'    as link;
+    select
+        'Direct Protocol Email System' as title,
+        '/dms/' as link;
+    select
+        'inbox' as title,
+        '/dms/inbox.sql' as link;
+    select
+        "subject" as title from inbox where CAST(id AS TEXT)=CAST($id AS TEXT);
+
+    select
+        'datagrid' as component;
+    select
+        'From' as title,
+        "from" as "description" from inbox where CAST(id AS TEXT)=CAST($id AS TEXT);
+    select
+        'To' as title,
+        "to" as "description"  from inbox where CAST(id AS TEXT)=CAST($id AS TEXT);
+    select
+        'Subject' as title,
+        "subject" as "description"  from inbox where CAST(id AS TEXT)=CAST($id AS TEXT);
+    select
+        'Date' as title,
+        "date" as "description"  from inbox where CAST(id AS TEXT)=CAST($id AS TEXT);
+
+    select 'datagrid' as component;
+      SELECT content AS description FROM inbox WHERE id = $id::TEXT;
+      SELECT 'table' AS component, 'attachment' AS markdown;
+      SELECT
+          CASE
+              WHEN attachment_filename LIKE '%.xml' OR attachment_mime_type = 'application/xml'
+              THEN '[' || attachment_filename || '](' || attachment_file_path || ' "download")' || ' | ' || '[View Details](/dms/patient-detail.sql?id=' || message_uid || ' "View Details")'
+              ELSE '[' || attachment_filename || '](' || attachment_file_path || ' "download")'
+          END AS "attachment"
+      FROM mail_content_attachment
+      WHERE CAST(message_uid AS TEXT) = CAST($id AS TEXT);
+
+    `;
+  }
+  @spn.shell({ breadcrumbsFromNavStmts: "no" })
+  "dms/patient-detail.sql"() {
+    return this.SQL`
+    SELECT
+    'breadcrumb' as component;
+    SELECT
+        'Home' as title,
+        '/'    as link;
+    SELECT
+        'Direct Protocol Email System' as title,
+        '/dms/' as link;
+    SELECT
+        'inbox' as title,
+        '/dms/inbox.sql' as link;
+    SELECT
+        '/dms/email-detail.sql?id=' || id as link,
+        "subject" as title from inbox where CAST(id AS TEXT)=CAST($id AS TEXT);
+    SELECT
+        first_name as title from patient_detail where CAST(message_uid AS TEXT)=CAST($id AS TEXT) ;
+
+   SELECT 'html' AS component, '
+  <link rel="stylesheet" href="/assets/style.css">
+  <h2>' || document_title || '</h2>
+  <table class="patient-summary">
+    <tr>
+      <th>Patient</th>
+      <td>' || first_name || ' ' || last_name || '<br>
+          <b>Patient-ID</b>: ' || id || ' (SSN) <b>Date of Birth</b>: ' || substr(birthTime, 7, 2) ||
+      CASE
+        WHEN strftime('%d', birthTime) IN ('01', '21', '31') THEN 'st'
+        WHEN strftime('%d', birthTime) IN ('02', '22') THEN 'nd'
+        WHEN strftime('%d', birthTime) IN ('03', '23') THEN 'rd'
+        ELSE 'th'
+      END || ' ' ||
+      CASE
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '1' THEN 'January'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '2' THEN 'February'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '3' THEN 'March'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '4' THEN 'April'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '5' THEN 'May'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '6' THEN 'June'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '7' THEN 'July'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '8' THEN 'August'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '9' THEN 'September'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '10' THEN 'October'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '11' THEN 'November'
+        WHEN cast(substr(birthTime, 5, 2) AS text) = '12' THEN 'December'
+        ELSE 'Invalid Month'
+      END || ' ' || substr(birthTime, 1, 4) || ' <b>Gender</b>: Female</td>
+    </tr>
+    <tr>
+      <th>Guardian</th>
+      <td>' || guardian_name || ' ' || guardian_family_name || ' - ' || guardian_display_name || '</td>
+    </tr>
+    <tr>'
+    ||
+    CASE
+      WHEN performer_name != '' THEN '
+      <th>Documentation Of</th>
+      <td><b>Performer</b>: ' || performer_name || ' ' || performer_family || ' ' || performer_suffix || ' <b>Organization</b>: ' || performer_organization || '</td>
+    </tr>'
+      ELSE ''
+    END ||'
+    <tr>
+      <th>Author</th>
+      <td>' || ad.author_given_names || ' ' || ad.author_family_names || ' ' || ad.author_suffixes || ', <b>Authored On</b>: ' ||
+      CASE
+        WHEN substr(author_times, 7, 2) = '01' THEN substr(author_times, 9, 2) || 'st '
+        WHEN substr(author_times, 7, 2) = '02' THEN substr(author_times, 9, 2) || 'nd '
+        WHEN substr(author_times, 7, 2) = '03' THEN substr(author_times, 9, 2) || 'rd '
+        ELSE substr(author_times, 9, 2) || 'th '
+      END ||
+      CASE
+        WHEN substr(author_times, 5, 2) = '01' THEN 'January '
+        WHEN substr(author_times, 5, 2) = '02' THEN 'February '
+        WHEN substr(author_times, 5, 2) = '03' THEN 'March '
+        WHEN substr(author_times, 5, 2) = '04' THEN 'April '
+        WHEN substr(author_times, 5, 2) = '05' THEN 'May '
+        WHEN substr(author_times, 5, 2) = '06' THEN 'June '
+        WHEN substr(author_times, 5, 2) = '07' THEN 'July '
+        WHEN substr(author_times, 5, 2) = '08' THEN 'August '
+        WHEN substr(author_times, 5, 2) = '09' THEN 'September '
+        WHEN substr(author_times, 5, 2) = '10' THEN 'October '
+        WHEN substr(author_times, 5, 2) = '11' THEN 'November '
+        WHEN substr(author_times, 5, 2) = '12' THEN 'December '
+        ELSE 'Invalid Month'
+      END ||
+      substr(author_times, 1, 4) || '</td>
+    </tr>
+    ' ||
+    CASE
+      WHEN ad.organization_names != '' THEN '
+      <tr>
+        <th>Author</th>
+        <td>' || ad.device_manufacturers || ' - ' || ad.device_software || ', Organization: ' || ad.organization_names || ', <b>Authored On</b>: ' ||
+        CASE
+          WHEN substr(author_times, 7, 2) = '01' THEN substr(author_times, 9, 2) || 'st '
+          WHEN substr(author_times, 7, 2) = '02' THEN substr(author_times, 9, 2) || 'nd '
+          WHEN substr(author_times, 7, 2) = '03' THEN substr(author_times, 9, 2) || 'rd '
+          ELSE substr(author_times, 9, 2) || 'th '
+        END ||
+        CASE
+          WHEN substr(author_times, 5, 2) = '01' THEN 'January '
+          WHEN substr(author_times, 5, 2) = '02' THEN 'February '
+          WHEN substr(author_times, 5, 2) = '03' THEN 'March '
+          WHEN substr(author_times, 5, 2) = '04' THEN 'April '
+          WHEN substr(author_times, 5, 2) = '05' THEN 'May '
+          WHEN substr(author_times, 5, 2) = '06' THEN 'June '
+          WHEN substr(author_times, 5, 2) = '07' THEN 'July '
+          WHEN substr(author_times, 5, 2) = '08' THEN 'August '
+          WHEN substr(author_times, 5, 2) = '09' THEN 'September '
+          WHEN substr(author_times, 5, 2) = '10' THEN 'October '
+          WHEN substr(author_times, 5, 2) = '11' THEN 'November '
+          WHEN substr(author_times, 5, 2) = '12' THEN 'December '
+        END ||
+        substr(author_times, 1, 4) || '</td>
+      </tr>'
+      ELSE ''
+    END || '
+  </table>' AS html
+FROM patient_detail pd
+JOIN author_detail ad ON pd.message_uid = ad.message_uid
+WHERE CAST(pd.message_uid AS TEXT) = CAST($id AS TEXT);
+
+    SELECT 'html' AS component, '
+      <link rel="stylesheet" href="/assets/style.css">
+      <table class="patient-details">
+      <tr>
+      <th class="no-border-bottom" style="background-color: #f2f2f2"><b>Document</b></th>
+      <td class="no-border-bottom" style="width:30%">
+        ID: '|| document_extension||' ('|| document_id||')<br>
+        Version:'|| version||'<br>
+        Set-ID: '|| set_id_extension||'
+      </td>
+      <th style="background-color: #f2f2f2"><b>Created On</b></th>
+      <td class="no-border-bottom"> ' ||
+      CASE
+          WHEN substr(custodian_time, 7, 2) = '01' THEN substr(custodian_time, 9, 2) || 'st '
+          WHEN substr(custodian_time, 7, 2) = '02' THEN substr(custodian_time, 9, 2) || 'nd '
+          WHEN substr(custodian_time, 7, 2) = '03' THEN substr(custodian_time, 9, 2) || 'rd '
+          ELSE substr(custodian_time, 9, 2) || 'th '
+      END ||
+      CASE
+          WHEN substr(custodian_time, 5, 2) = '01' THEN 'January '
+          WHEN substr(custodian_time, 5, 2) = '02' THEN 'February '
+          WHEN substr(custodian_time, 5, 2) = '03' THEN 'March '
+          WHEN substr(custodian_time, 5, 2) = '04' THEN 'April '
+          WHEN substr(custodian_time, 5, 2) = '05' THEN 'May '
+          WHEN substr(custodian_time, 5, 2) = '06' THEN 'June '
+          WHEN substr(custodian_time, 5, 2) = '07' THEN 'July '
+          WHEN substr(custodian_time, 5, 2) = '08' THEN 'August '
+          WHEN substr(custodian_time, 5, 2) = '09' THEN 'September '
+          WHEN substr(custodian_time, 5, 2) = '10' THEN 'October '
+          WHEN substr(custodian_time, 5, 2) = '11' THEN 'November '
+          WHEN substr(custodian_time, 5, 2) = '12' THEN 'December '
+      END ||
+      substr(custodian_time, 1, 4) || '</td>
+      </tr>
+      <tr>
+        <th style="background-color: #f2f2f2"><b>Custodian</b></th>
+        <td>'|| custodian||'</td>
+        <th style="background-color: #f2f2f2"><b>Contact Details</b></th>
+        <td>
+          Workplace: '|| custodian_address_line1||' '|| custodian_city||', '|| custodian_state||' '|| custodian_postal_code||'<br> '|| custodian_country||'<br>
+          Tel Workplace: '|| custodian_telecom||'
+        </td>
+      </tr>
+    </table>'AS html
+    FROM patient_detail
+    WHERE CAST(message_uid AS TEXT)=CAST($id AS TEXT);
+
+    SELECT 'html' AS component, '
+    <link rel="stylesheet" href="/assets/style.css">
+    <style>
+      .patient-details {
+        width: 100%;
+        border-collapse: collapse;
+        margin-bottom: 20px;
+        font-family: Arial, sans-serif;
+      }
+
+    </style>
+
+    <table class="patient-details">
+      <tr>
+        <th class="no-border-bottom" ><b>Patient</b></th>
+        <td class="no-border-bottom" style="width:30%">'|| first_name||'  '|| last_name||'</td>
+        <th class="no-border-bottom" ><b>Contact Details</b></th>
+        <td class="no-border-bottom">'|| address||' '|| city||', '|| state||' '|| postalCode||' '|| addr_use||'</td>
+      </tr>
+      <tr>
+        <th class="no-border-bottom" ><b>Date of Birth</b></th>
+        <td class="no-border-bottom">'|| strftime('%Y-%m-%d', substr(birthTime, 1, 4) || '-' || substr(birthTime, 5, 2) || '-' || substr(birthTime, 7, 2))||' </td>
+        <th class="no-border-bottom" ><b>Gender</b></th>
+        <td class="no-border-bottom">'|| CASE
+        WHEN gender_code = 'F' THEN 'Female'
+        WHEN gender_code = 'M' THEN 'Male'
+        ELSE 'Other'
+      END||'</td>
+      </tr>
+      <tr>
+        <th class="no-border-bottom" ><b>Race</b></th>
+        <td class="no-border-bottom">'||race_displayName||'</td>
+        <th class="no-border-bottom" ><b>Ethnicity</b></th>
+        <td class="no-border-bottom">'||ethnic_displayName||'</td>
+      </tr>
+      <tr>
+        <th class="no-border-bottom" ><b>Patient-IDs</b></th>
+        <td class="no-border-bottom">'||id||'</td>
+        <th class="no-border-bottom" ><b>Language Communication</b></th>
+        <td class="no-border-bottom">'||
+        CASE
+            WHEN language_code IS NOT NULL THEN language_code
+            ELSE 'Not Given'
+        END
+        ||'</td>
+      </tr>
+
+      <tr>
+        <th class="no-border-bottom" ><b>Guardian</b></th>
+        <td class="no-border-bottom">'||guardian_name||' '||guardian_family_name||' '||guardian_display_name||'</td>
+        <th class="no-border-bottom" ><b>Contact Details</b></th>
+        <td class="no-border-bottom">'|| guardian_address||', '|| guardian_city||' ,'|| guardian_state||', '|| guardian_zip||' ,'|| guardian_country||'</td>
+      </tr>
+
+      <tr>
+        <th class="no-border-bottom" ><b>Provider Organization</b></th>
+        <td class="no-border-bottom">'||provider_organization||'</td>
+        <th class="no-border-bottom" ><b>Contact Details (Organization)</b></th>
+        <td class="no-border-bottom">'|| provider_address_line||', '|| provider_city||' ,'|| provider_state||', '|| provider_country||' ,'|| provider_zip||'</td>
+      </tr>
+
+
+    </table> 'AS html
+  FROM patient_detail
+  WHERE CAST(message_uid AS TEXT)=CAST($id AS TEXT);
+
+
+  select 'html' as component;
+  select '<link rel="stylesheet" href="/assets/style.css">
+    <details class="accordian-head">
+  <summary>'||section_title||'</summary>
+  <div class="patient-details">
+    <div>'||table_data||'</div>
+  </div>
+  </details>' as html
+  FROM patient_diagnosis
+  WHERE CAST(message_uid AS TEXT)=CAST($id AS TEXT);
+  `;
+  }
+  @dmsNav({
+    caption: "Dispatched",
+    description: "",
+    siblingOrder: 2,
+  })
+  "dms/dispatched.sql"() {
+    return this.SQL`
+      ${this.activePageTitle()}
+
+      SELECT 'table' as component,
+            'subject' AS markdown,
+            'Column Count' as align_right,
+            TRUE as sort,
+            TRUE as search;
+      SELECT * from phimail_delivery_detail where status='dispatched'`;
+  }
+
+  @dmsNav({
+    caption: "Failed",
+    description: "",
+    siblingOrder: 2,
+  })
+  "dms/failed.sql"() {
+    return this.SQL`
+      ${this.activePageTitle()}
+
+      SELECT 'table' as component,
+            'subject' AS markdown,
+            'Column Count' as align_right,
+            TRUE as sort,
+            TRUE as search;
+      SELECT * from phimail_delivery_detail where status!='dispatched'`;
+  }
+}
+
+export async function SQL() {
+  return await spn.TypicalSqlPageNotebook.SQL(
+    new class extends spn.TypicalSqlPageNotebook {
+      async statelessDmsSQL() {
+        // read the file from either local or remote (depending on location of this file)
+        return await spn.TypicalSqlPageNotebook.fetchText(
+          import.meta.resolve("./stateless-dms.surveilr.sql"),
+        );
+      }
+    }(),
+    new sh.ShellSqlPages(),
+    new c.ConsoleSqlPages(),
+    new ur.UniformResourceSqlPages(),
+    new orch.OrchestrationSqlPages(),
+    new DirectMessageSqlPages(),
+  );
+}
+
+// this will be used by any callers who want to serve it as a CLI with SDTOUT
+if (import.meta.main) {
+  console.log((await SQL()).join("\n"));
+}
---
+++ src/content/patterns/en/direct-messaging-service.md
@@ -0,0 +1,59 @@
+---
+title: "Direct Secure Messaging Service"
+description: "Secure exchange of clinical data using the phiMail service"
+main:
+  id: 5
+  content: |
+    The Direct Secure Messaging Service Pattern for surveilr ingests clinical data CCDA XML files and allows querying, summarizing episodes of care and exploration of those files.
+  imgCard: "@/images/pattern/direct-message-pattern.avif"
+  imgMain: "@/images/pattern/direct-message-pattern.png"
+  imgAlt: "Direct Secure Messaging Service"
+tabs:
+  - id: "tabs-with-card-item-1"
+    dataTab: "#tabs-with-card-1"
+    title: "Overview"
+  - id: "tabs-with-card-item-2"
+    dataTab: "#tabs-with-card-2"
+    title: "Capabilities"
+  - id: "tabs-with-card-item-3"
+    dataTab: "#tabs-with-card-3"
+    title: "Screenshots"
+longDescription:
+  title: "Direct Secure Messaging Service"
+  subTitle: |
+    This pattern is designed to facilitate the secure exchange of clinical data using the phiMail service. It consists of four main modules, each serving a distinct purpose within the overall system. These modules work together to securely and efficiently send, receive, process, and analyze clinical messages.
+
+
+    PhiMail is a secure messaging service that facilitates the exchange of protected health information (PHI) in compliance with healthcare regulations like HIPAA. It is designed to support healthcare providers, organizations, and other stakeholders in securely transmitting health-related data, ensuring both privacy and integrity.
+  btnTitle: "Use the SQL"
+  btnURL: "/lib/pattern/direct-messaging-service/package.sql"
+descriptionList:
+  - title: "Compliance with HIPAA"
+    subTitle: "PhiMail ensures that all communications involving PHI meet the stringent requirements of the Health Insurance Portability and Accountability Act (HIPAA). This includes encryption of data in transit, secure storage, and proper handling of sensitive information."
+  - title: "DIRECT Protocol Support"
+    subTitle: "PhiMail is built on the DIRECT protocol, a standardized method for secure email communication in healthcare. This enables seamless and secure transmission of health information between different systems and organizations, such as hospitals, laboratories, and physician offices."
+  - title: "Encryption and Security"
+    subTitle: "PhiMail utilizes robust encryption mechanisms to protect data, ensuring that only authorized parties can access the information. This includes the use of SSL/TLS for secure connections and digital signatures to verify the authenticity of messages."
+  - title: "Interoperability"
+    subTitle: "PhiMail supports interoperability with various healthcare systems, enabling the exchange of data in different formats like CDA (Clinical Document Architecture). This allows for smooth integration with electronic health records (EHR) systems and other health information exchanges (HIEs)."
+specificationsLeft:
+  - title: "Message Tracking and Acknowledgments"
+    subTitle: "phiMail provides detailed tracking of message delivery, including delivery status and read receipts. This is crucial in healthcare settings, where confirmation of receipt and action on health-related messages is often required."
+  - title: "Scalability and Customization"
+    subTitle: "phiMail can be tailored to the specific needs of healthcare organizations, whether they require a standalone messaging solution or integration with existing systems. It is scalable to accommodate the needs of small practices as well as large healthcare networks."
+  - title: "Secure Attachments"
+    subTitle: "phiMail supports the secure exchange of attachments, such as medical records, lab results, and other documents. These attachments are encrypted and can be managed securely within the platform."
+
+specificationsRight:
+  - title: "Clinical Communication"
+    subTitle: "Facilitates the exchange of medical records, referrals, and other critical health information between providers, ensuring timely and secure communication."
+  - title: "Laboratory Results"
+    subTitle: "Enables labs to send results directly to providers, integrating seamlessly with EHR systems for quick access."
+  - title: "Patient Communication"
+    subTitle: "Allows healthcare providers to securely send and receive information directly with patients, such as test results, care instructions, or appointment reminders."
+
+blueprints:
+  first: "@/images/pattern/direct-message-ss-2.avif"
+  second: "@/images/pattern/direct-message-ss-3.avif"
+  third: "@/images/pattern/direct-message-ss-1.avif"
+---
