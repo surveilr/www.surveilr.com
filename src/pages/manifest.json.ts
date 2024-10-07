7d908c696c267ab3b3fd7402243ae56e6ac1f05f
Ajay Mohan.K <ajay.kuruvath@citrusinformatics.com>
2024-10-07 11:42:42 +0530

    chore: replace all screwfast reference #53

--- src/pages/manifest.json.ts
+++ src/pages/manifest.json.ts
@@ -44,8 +44,8 @@ export const GET: APIRoute = async () => {
   );
 
   const manifest = {
-    short_name: "ScrewFast",
-    name: "ScrewFast",
+    short_name: "Survelir",
+    name: "Survelir",
     icons,
     display: "minimal-ui",
     id: "/",
