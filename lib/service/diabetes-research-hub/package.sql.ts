c35d8f6b111b1abe15a86fdb088b9869f9226275
Anitha Varghese <anitha.varghese@citrusinformatics.com>
2024-10-15 12:41:40 +0530

    Update: Modify automation script to use  `deno2.0` and integrate latest `sqlite3` import to support SQRT #29

--- lib/service/diabetes-research-hub/package.sql.ts
+++ lib/service/diabetes-research-hub/package.sql.ts
@@ -1039,27 +1039,42 @@ Participants are individuals who volunteer to take part in CGM research studies.
 export async function drhSQL() {
   return await spn.TypicalSqlPageNotebook.SQL(
     new class extends spn.TypicalSqlPageNotebook {
-      async deidentifyDRHSQL() {
-        // read the file from either local or remote (depending on location of this file)
-        return await spn.TypicalSqlPageNotebook.fetchText(
-          import.meta.resolve(
-            "./orchestration/deidentification-orchestration.sql",
-          ),
-        );
-      }
+
+      
+      // async deidentifyDRHSQL() {
+      //   // This function retrieves the SQL script for data de-identification.
+      //   // Note: Deidentification functions are only available through the `surveilr shell` or `surveilr orchestrate` commands.
+      //   // Issues prevail while executing these commands on Windows OS.
+      //   return await spn.TypicalSqlPageNotebook.fetchText(
+      //     import.meta.resolve(
+      //       "./orchestration/deidentification-orchestration.sql",
+      //     ),
+      //   );
+      // }
 
       async vandvDRHSQL() {
-        // read the file from either local or remote (depending on location of this file)
+        // This function retrieves the SQL script for verfication and validation
         return await spn.TypicalSqlPageNotebook.fetchText(
           import.meta.resolve("./orchestration/vv-orchestration.sql"),
         );
       }
+
       async statelessDRHSQL() {
         // read the file from either local or remote (depending on location of this file)
         return await spn.TypicalSqlPageNotebook.fetchText(
           import.meta.resolve("./stateless.sql"),
         );
       }
+
+      
+      // async metricsDRHSQL() {
+      //   // This function fetches the SQL query for DRH metrics after the combined view has been generated.
+      //   // Ensure that the combined view is created before calling this function.        
+      //   return await spn.TypicalSqlPageNotebook.fetchText(
+      //     import.meta.resolve("./drh-metrics.sql"),
+      //   );
+      // }
+
     }(),
     // new sh.ShellSqlPages(),
     new DrhShellSqlPages(),
