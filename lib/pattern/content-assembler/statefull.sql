50a488c04fc0983f8c20340e20e217b0713313b4
Joby James <joby.james@citrusinformatics.in>
2024-10-28 13:32:05 +0530

    feat: add content assembler statefull table and stateless view #20

--- lib/pattern/content-assembler/statefull.sql
+++ lib/pattern/content-assembler/statefull.sql
@@ -1,10 +1,10 @@
-DROP TABLE IF EXISTS ur_transform_html_email_anchor;
-CREATE TABLE ur_transform_html_email_anchor AS
+DROP TABLE IF EXISTS ur_transform_html_flattened_email_anchor;
+CREATE TABLE ur_transform_html_flattened_email_anchor AS
 SELECT
     uniform_resource_transform_id,
     uniform_resource_id,
     json_extract(json_each.value, '$.attributes.href') AS anchor,
-    json_extract(json_each.value, '$.children[0]') AS text
+    json_extract(json_each.value, '$.children[0]') AS anchor_text
 FROM
     uniform_resource_transform,
     json_each(content);
@@ -17,9 +17,26 @@ SELECT
     uniform_resource_id,
     anchor,
     CASE
-        WHEN regexp_like(anchor, '(?i)optout') THEN 'optout'
-        WHEN regexp_like(anchor, '(?i)unsubscribe') THEN 'unsubscribe'
-    END AS type,
-    text
+        WHEN regexp_like(anchor, '(?i)unsubscribe|list-unsubscribe') THEN 'Unsubscribe'
+        WHEN regexp_like(anchor, '(?i)optout|opt-out') THEN 'Optout'
+        WHEN regexp_like(anchor, '(?i)preferences') THEN 'Preferences'
+        WHEN regexp_like(anchor, '(?i)remove') THEN 'Remove'
+        WHEN regexp_like(anchor, '(?i)manage') THEN 'Manage'
+        WHEN regexp_like(anchor, '(?i)email-settings') THEN 'Email-settings'
+        WHEN regexp_like(anchor, '(?i)subscription|subscribe') THEN 'Subscribe'
+        WHEN regexp_like(anchor, '(?i)mailto:') THEN 'mailto'
+    END AS anchor_type,
+    anchor_text
 FROM
-    ur_transform_html_email_anchor;
+    ur_transform_html_flattened_email_anchor;
+
+DROP TABLE IF EXISTS ur_transform_html_email_anchor;
+CREATE TABLE ur_transform_html_email_anchor AS
+SELECT
+    uniform_resource_transform_id,
+    uniform_resource_id,
+    anchor,
+    anchor_text
+FROM
+    ur_transform_html_flattened_email_anchor
+    WHERE NOT regexp_like(anchor, '(?i)unsubscribe|optout|opt-out|preferences|remove|manage|email-settings|subscription|subscribe|list-unsubscribe|mailto:');
