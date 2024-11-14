e8be20b9ae61810456ac2fd8b2733b448406fc50
Joby James <joby.james@citrusinformatics.in>
2024-11-14 14:42:22 +0530

    feat: periodical filtered url list #44

--- lib/pattern/content-assembler/stateless.sql
+++ lib/pattern/content-assembler/stateless.sql
@@ -1,20 +1,32 @@
-drop view if exists inbox;
-CREATE VIEW inbox AS
-SELECT
-    ur_base.uniform_resource_id AS base_uniform_resource_id,
-    ur_imap."from" AS message_from,
-    ur_imap."subject" AS message_subject,
-    ur_imap."date" AS message_date,
-    ur_extended.uniform_resource_id AS extended_uniform_resource_id,
-    ur_extended.uri AS extended_uri
-FROM
-    ur_ingest_session_imap_acct_folder_message ur_imap
-JOIN
-    uniform_resource ur_base
-    -- the `uniform_resource` table is connected to the `ur_ingest_session_imap_acct_folder_message` table through a foreign key
-    ON ur_base.ingest_session_imap_acct_folder_message = ur_imap.ur_ingest_session_imap_acct_folder_message_id
-JOIN
-    uniform_resource ur_extended
-    ON ur_extended.uri = ur_base.uri || '/html'
-WHERE
-    ur_extended.uri LIKE '%/html';
+DROP VIEW IF EXISTS periodicals_from;
+CREATE VIEW periodicals_from AS
+    SELECT
+        message_from
+    FROM
+        ur_periodical_chached GROUP BY message_from;
+
+DROP VIEW IF EXISTS periodicals_subject;
+CREATE VIEW periodicals_subject AS
+    SELECT
+    periodical_uniform_resource_id,
+    message_from,
+    message_to,
+    message_subject,
+    message_date
+    FROM
+        ur_periodical_chached;
+
+
+DROP VIEW IF EXISTS periodical_anchor;
+CREATE VIEW periodical_anchor AS
+    SELECT
+        eac.uniform_resource_id,
+        eac.uniform_resource_transform_id,
+        eac.anchor as orginal_url,
+        acc.canonical_link,
+        lnktxt.url_text
+    FROM
+        ur_transform_html_email_anchor_cached eac
+    INNER JOIN ur_periodical_anchor_text_cached lnktxt ON lnktxt.anchor=eac.anchor
+    INNER JOIN ur_transform_html_email_anchor_canonical_cached acc ON acc.anchor=eac.anchor;
+
