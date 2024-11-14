a9425a53d3d52d564c941164a1ab747e4aa41372
geovlazar <geovlazar@users.noreply.github.com>
2024-11-14 09:19:27 +0000

    deploy: 893d6d147540e529d407339fb19f64c9e3be034f

--- lib/pattern/content-assembler/package.sql.ts
+++ lib/pattern/content-assembler/package.sql.ts
@@ -42,8 +42,9 @@ export class ContentAssemblerSqlPages extends spn.TypicalSqlPageNotebook {
   }
 
   @spn.navigationPrimeTopLevel({
-    caption: "Content Assembler IMAP",
-    description: "Email system with IMAP",
+    caption: "Content Assembler",
+    description:
+      "The Content Assembler harnesses pre-curated content from influencers, curators, and authoritative sources, collecting, de-duplicating, and scoring valuable links shared across platforms like email, Twitter, and LinkedIn for reuse in B2B and community channels in Surveilr.",
   })
   "cak/index.sql"() {
     return this.SQL`
@@ -61,158 +62,224 @@ export class ContentAssemblerSqlPages extends spn.TypicalSqlPageNotebook {
   }
 
   @cakNav({
-    caption: "IMAP Accounts",
+    caption: "Periodicals",
     description: ``,
     siblingOrder: 1,
   })
-  "cak/accounts.sql"() {
+  "cak/periodicals.sql"() {
+    const viewName = `periodicals_from`;
+    const pagination = this.pagination({
+      tableOrViewName: viewName,
+    });
     return this.SQL`
       ${this.activePageTitle()}
 
+      -- sets up $limit, $offset, and other variables (use pagination.debugVars() to see values in web-ui)
+      ${pagination.init()}
+
+       select
+        'text'              as component,
+        'The Source List page provides a streamlined view of all collected content sources. This page displays only the origins of the content, such as sender information for email sources, making it easy to see where each piece of content came from. Use this list to quickly review and identify the various sources contributing to the curated content collection.' as contents;
+
+      -- Display uniform_resource table with pagination
       SELECT 'table' AS component,
             'subject' AS markdown,
             'Column Count' as align_right,
             TRUE as sort,
             TRUE as search,
-            'accounts' AS markdown;
+            'from' AS markdown;
 
        SELECT
-        '[' || email || '](/cak/account-folder.sql?imap_account_id=' || ur_ingest_session_imap_account_id || ')' AS "accounts"
-          FROM uniform_resource_imap
-          GROUP BY ur_ingest_session_imap_account_id
-          ORDER BY uniform_resource_id;
+        '[' || message_from || '](/cak/periodicals_subject.sql?message_from=' || message_from || ')' AS "from"
+          FROM ${viewName}
+          LIMIT $limit
+        OFFSET $offset;
+        ${pagination.renderSimpleMarkdown()}
       `;
   }
 
   @spn.shell({ breadcrumbsFromNavStmts: "no" })
-  "cak/account-folder.sql"() {
+  "cak/periodicals_subject.sql"() {
+    const viewName = `periodicals_subject`;
+    const pagination = this.pagination({
+      tableOrViewName: viewName,
+      whereSQL: "WHERE message_from=$message_from",
+    });
     return this.SQL`
-    SELECT 'breadcrumb' as component;
+    --- Display breadcrumb
+     SELECT
+        'breadcrumb' AS component;
       SELECT
-          'Home' as title,
-          '/'    as link;
+        'Home' AS title,
+        '/'    AS link;
       SELECT
-          'Content Assembler IMAP' as title,
-          '/cak' as link;
+        'Content Assembler' AS title,
+        '/cak' AS link;
       SELECT
-          'IMAP Accounts' as title,
-          '/cak/accounts.sql' as link;
-      SELECT
-          (SELECT
-        'Folder (' || email || ')'
-          FROM uniform_resource_imap WHERE ur_ingest_session_imap_account_id = $imap_account_id::TEXT
-          GROUP BY ur_ingest_session_imap_account_id
-          ORDER BY uniform_resource_id) as title,
-          '/cak/account-folder.sql?imap_account_id=' || $imap_account_id::TEXT as link;
+        'Periodicals' AS title,
+        '/cak/periodicals.sql' AS link;
+      SELECT $message_from AS title, '/cak/periodicals_subject.sql?message_from='|| $message_from  AS link;
 
-        SELECT
+      --- Dsply Page Title
+      SELECT
           'title'   as component,
-          (SELECT
-        'Folder (' || email || ')'
-          FROM uniform_resource_imap WHERE ur_ingest_session_imap_account_id = $imap_account_id::TEXT
-          GROUP BY ur_ingest_session_imap_account_id
-          ORDER BY uniform_resource_id) as contents;
+          'Periodicals From ' || $message_from as contents;
 
-      SELECT 'table' AS component,
-            'subject' AS markdown,
-            'Column Count' as align_right,
-            TRUE as sort,
-            TRUE as search,
-            'folder' AS markdown;
-
-     SELECT '[' || folder_name || '](/cak/imap-mail-list.sql?folder_id=' || ur_ingest_session_imap_acct_folder_id || ')' AS "folder"
-        FROM uniform_resource_imap
-        WHERE ur_ingest_session_imap_account_id=$imap_account_id::TEXT
-        GROUP BY ur_ingest_session_imap_acct_folder_id
-        ORDER BY uniform_resource_id;
-      `;
-  }
-
-  @spn.shell({ breadcrumbsFromNavStmts: "no" })
-  "cak/imap-mail-list.sql"() {
-    return this.SQL`
-    SELECT 'breadcrumb' as component;
-    SELECT
-          'Home' as title,
-          '/'    as link;
-      SELECT
-          'Content Assembler IMAP' as title,
-          '/cak' as link;
-      SELECT
-          'IMAP Accounts' as title,
-          '/cak/accounts.sql' as link;
-      SELECT
-          (SELECT
-        'Folder (' || email || ')'
-          FROM uniform_resource_imap WHERE ur_ingest_session_imap_acct_folder_id = $folder_id::TEXT
-          GROUP BY ur_ingest_session_imap_account_id
-          ORDER BY uniform_resource_id) as title,
-          '/cak/account-folder.sql?imap_account_id=' || (SELECT
-        ur_ingest_session_imap_account_id
-          FROM uniform_resource_imap WHERE ur_ingest_session_imap_acct_folder_id = $folder_id::TEXT
-          GROUP BY ur_ingest_session_imap_account_id) as link;
-
-      SELECT
-          (
-        SELECT email || ' (' || folder_name || ')'
-          FROM uniform_resource_imap WHERE ur_ingest_session_imap_acct_folder_id = $folder_id::TEXT
-          GROUP BY ur_ingest_session_imap_account_id
-          ORDER BY uniform_resource_id) as title,
-          '/cak/imap-mail-list.sql?folder_id=' || $folder_id::TEXT as link;
-
-
-      SELECT 'table' AS component,
-            'Uniform Resources' AS title,
-            "Size (bytes)" as align_right,
-            TRUE AS sort,
-            TRUE AS search,
-            TRUE AS hover,
-            TRUE AS striped_rows,
-            TRUE AS small;
-
-     SELECT subject,"from",
-     CASE
-          WHEN ROUND(julianday('now') - julianday(date)) = 0 THEN 'Today'
-          WHEN ROUND(julianday('now') - julianday(date)) = 1 THEN '1 day ago'
-          WHEN ROUND(julianday('now') - julianday(date)) BETWEEN 2 AND 6 THEN CAST(ROUND(julianday('now') - julianday(date)) AS INT) || ' days ago'
-          WHEN ROUND(julianday('now') - julianday(date)) < 30 THEN CAST(ROUND(julianday('now') - julianday(date)) AS INT) || ' days ago'
-          WHEN ROUND(julianday('now') - julianday(date)) < 365 THEN CAST(ROUND((julianday('now') - julianday(date)) / 30) AS INT) || ' months ago'
-          ELSE CAST(ROUND((julianday('now') - julianday(date)) / 365) AS INT) || ' years ago'
-      END AS "Relative Time",
-      strftime('%Y-%m-%d', substr(date, 1, 19)) as date
-      FROM uniform_resource_imap
-      WHERE ur_ingest_session_imap_acct_folder_id=$folder_id::TEXT
-      ORDER BY uniform_resource_id;
-      `;
-  }
-
-  @spn.shell({ breadcrumbsFromNavStmts: "no" })
-  "cak/email-detail.sql"() {
-    return this.SQL`
-      select
-      'breadcrumb' as component;
-      select
-          'Home' as title,
-          '/'    as link;
-      select
-          'IMAP Email System' as title,
-          '/cak/' as link;
-      select
-          'inbox' as title,
-          '/cak/inbox.sql' as link;
-      select
-          "message_subject" as title from inbox where CAST(extended_uniform_resource_id AS TEXT)=CAST($id AS TEXT);
-
-      SELECT 'table' AS component,
-                  'Column Count' as align_right,
-                  TRUE as sort,
-                  TRUE as search;
+      --- Dsply Page Description
        SELECT
-        uniform_resource_id,
-        anchor as "News letter link",
-        anchor_text as "link text"
-        from
-          ur_transform_html_email_anchor where CAST(uniform_resource_id AS TEXT)=CAST($id AS TEXT);`;
+    'text' AS component,
+    'The Source Details page offers an in-depth view of content collected from a specific sender. Here, you''ll find a list of messages from this source, including each message''s subject, sender, recipient, and send date. This organized layout helps you explore all communications from this source in one place, making it easy to review and track relevant content by date and topic.' AS contents;
+
+       -- sets up $limit, $offset, and other variables (use pagination.debugVars() to see values in web-ui)
+      ${pagination.init()}
+
+      -- Display uniform_resource table with pagination
+      SELECT 'table' AS component,
+          'Column Count' as align_right,
+          TRUE as sort,
+          TRUE as search,
+          'subject' AS markdown;
+
+      SELECT
+        '[' || message_subject || '](/cak/periodical_anchor.sql?periodical_uniform_resource_id=' || periodical_uniform_resource_id || ')' AS "subject",
+        message_from as "from",
+        message_to as "to",
+         CASE
+          WHEN ROUND(julianday('now') - julianday(message_date)) = 0 THEN 'Today'
+          WHEN ROUND(julianday('now') - julianday(message_date)) = 1 THEN '1 day ago'
+          WHEN ROUND(julianday('now') - julianday(message_date)) BETWEEN 2 AND 6 THEN CAST(ROUND(julianday('now') - julianday(message_date)) AS INT) || ' days ago'
+          WHEN ROUND(julianday('now') - julianday(message_date)) < 30 THEN CAST(ROUND(julianday('now') - julianday(message_date)) AS INT) || ' days ago'
+          WHEN ROUND(julianday('now') - julianday(message_date)) < 365 THEN CAST(ROUND((julianday('now') - julianday(message_date)) / 30) AS INT) || ' months ago'
+          ELSE CAST(ROUND((julianday('now') - julianday(message_date)) / 365) AS INT) || ' years ago'
+      END AS "Relative Time"
+      FROM ${viewName}
+      WHERE message_from=$message_from::TEXT
+      LIMIT $limit
+      OFFSET $offset;
+      ${pagination.renderSimpleMarkdown("message_from")};
+      `;
+  }
+
+  @spn.shell({ breadcrumbsFromNavStmts: "no" })
+  "cak/periodical_anchor.sql"() {
+    return this.SQL`
+
+    --- Display breadcrumb
+     SELECT
+        'breadcrumb' AS component;
+      SELECT
+        'Home' AS title,
+        '/'    AS link;
+      SELECT
+        'Content Assembler' AS title,
+        '/cak' AS link;
+      SELECT
+        'Periodicals' AS title,
+        '/cak/periodicals.sql' AS link;
+      SELECT message_from AS title, '/cak/periodicals_subject.sql?message_from='|| message_from  AS link FROM periodicals_subject WHERE periodical_uniform_resource_id = $periodical_uniform_resource_id::TEXT;
+
+      SELECT
+        message_subject as title,
+        '/cak/periodical_anchor.sql?periodical_uniform_resource_id='|| periodical_uniform_resource_id AS link
+      FROM
+        periodicals_subject
+      WHERE
+        periodical_uniform_resource_id = $periodical_uniform_resource_id::TEXT;
+
+    --- Dsply Page Title
+      SELECT
+        'title' as component,
+        message_subject as contents
+      FROM
+        periodicals_subject
+      WHERE
+        periodical_uniform_resource_id = $periodical_uniform_resource_id::TEXT;
+
+
+    select
+    'text'              as component,
+    'The Newsletter Link Details page provides a comprehensive list of URLs shared within a specific newsletter. For each entry, you’ll find the original URL as it appeared in the newsletter, the link text, and the canonical URL (standardized for consistent reference). This page also includes key metadata for each link, such as title, description, and any additional structured data, allowing for an in-depth look at the content and context of each link. This organized view makes it easy to analyze and manage all linked resources from the newsletter.' as contents;
+
+     SELECT 'table' AS component,
+          'Column Count' as align_right,
+          TRUE as sort,
+          TRUE as search,
+          'canonical link' AS markdown,
+          'meta data' AS markdown,
+          'original link url' AS markdown;
+      SELECT
+        '[' || url_text || ']('|| orginal_url ||')'   AS "original link url",
+        canonical_link as 'canonical link',
+        '[ Meta Data ](/cak/periodicals_meta.sql?url=' || orginal_url || ')' AS "meta data"
+      FROM
+        periodical_anchor
+      WHERE
+        uniform_resource_id = $periodical_uniform_resource_id::TEXT
+    `;
+  }
+
+  @spn.shell({ breadcrumbsFromNavStmts: "no" })
+  "cak/periodicals_meta.sql"() {
+    return this.SQL`
+
+    --- Display breadcrumb
+     SELECT
+        'breadcrumb' AS component;
+      SELECT
+        'Home' AS title,
+        '/'    AS link;
+      SELECT
+        'Content Assembler' AS title,
+        '/cak' AS link;
+      SELECT
+        'Periodicals' AS title,
+        '/cak/periodicals.sql' AS link;
+      SELECT
+            ps.message_from AS title,
+            '/cak/periodicals_subject.sql?message_from='|| ps.message_from AS link
+            FROM
+              periodicals_subject ps
+            INNER JOIN periodical_anchor pa ON pa.uniform_resource_id = ps.periodical_uniform_resource_id
+            WHERE
+              pa.orginal_url = $url::TEXT;
+
+      SELECT
+        ps.message_subject as title,
+        '/cak/periodical_anchor.sql?periodical_uniform_resource_id='|| ps.periodical_uniform_resource_id AS link
+      FROM
+        periodicals_subject ps
+      INNER JOIN periodical_anchor pa ON pa.uniform_resource_id = ps.periodical_uniform_resource_id
+      WHERE
+        pa.orginal_url = $url::TEXT;
+
+      SELECT
+        'Meta Data' AS title,
+        '/cak/periodicals_meta.sql?url='|| $url AS link;
+
+    --- Dsply Page Title
+      SELECT
+          'title'   as component,
+          'Meta Data ('|| ps.message_subject || ')'  as contents
+          FROM periodicals_subject ps
+          INNER JOIN periodical_anchor pa ON pa.uniform_resource_id = ps.periodical_uniform_resource_id
+          WHERE pa.orginal_url = $url::TEXT;
+
+    select
+    'text'              as component,
+    'The Link Metadata Viewer page offers a detailed look at the metadata and Open Graph properties of each newsletter link. For every link, this page displays essential metadata fields like title, description, and keywords, as well as Open Graph data such as images, type, and URL, providing a rich context for the content. This structured view allows you to easily understand the attributes and presentation of each link, making it an invaluable tool for analyzing and curating high-quality, shareable content from newsletters.' as contents;
+
+     SELECT 'table' AS component,
+          'Column Count' as align_right,
+          TRUE as sort,
+          TRUE as search;
+      SELECT
+        property_name as property,
+        content
+      FROM
+        ur_transform_html_email_anchor_meta_cached
+      WHERE
+        anchor = $url::TEXT AND property_name IS NOT NULL
+    `;
   }
 }
 
