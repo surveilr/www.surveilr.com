5ff949d2202eaf1a3c5e865e109ade7e0b49597a
MeetAnithaVarghese <MeetAnithaVarghese@users.noreply.github.com>
2024-11-29 08:30:52 +0000

    deploy: 5580843a836b9b628c7a2dfbd9c7c09d0493239d

---
+++ lib/service/diabetes-research-hub/drh-basepackage.sql
@@ -0,0 +1,2 @@
+Error executing service/diabetes-research-hub/drh-basepackage.sql: Command failed: deno run -A lib/service/diabetes-research-hub/drh-basepackage.sql.ts
+[0m[1m[31merror[0m: Unsupported lockfile version '4'. Try upgrading Deno or recreating the lockfile at '/home/runner/work/www.surveilr.com/www.surveilr.com/deno.lock'.
--- lib/service/diabetes-research-hub/package.sql
+++ lib/service/diabetes-research-hub/package.sql
@@ -1,2 +1,2 @@
-Error executing service/diabetes-research-hub/package.sql: Command failed: lib/service/diabetes-research-hub/package.sql.ts
+Error executing service/diabetes-research-hub/package.sql: Command failed: deno run -A lib/service/diabetes-research-hub/package.sql.ts
 [0m[1m[31merror[0m: Unsupported lockfile version '4'. Try upgrading Deno or recreating the lockfile at '/home/runner/work/www.surveilr.com/www.surveilr.com/deno.lock'.
