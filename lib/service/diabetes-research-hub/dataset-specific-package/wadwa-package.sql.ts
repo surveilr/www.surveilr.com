558f1e712b16f17865cd8f00327b288023b3b604
Siju Joseph <siju.joseph@citrusinformatics.com>
2024-12-10 16:38:44 +0530

    feat: add wadwa study #29

---
+++ lib/service/diabetes-research-hub/dataset-specific-package/wadwa-package.sql.ts
@@ -0,0 +1,45 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-run --allow-sys --allow-ffi
+import { sqlPageNB as spn } from "../deps.ts";
+import * as pkg from "../drh-basepackage.sql.ts";
+
+export class wadaSqlPages extends spn.TypicalSqlPageNotebook {
+  //metrics static views shall be generated after the combined_cgm_tracing is created.
+  async statelessMetricsSQL() {
+    // stateless SQL for the metrics
+    return await spn.TypicalSqlPageNotebook.fetchText(
+      import.meta.resolve(
+        "../drh-metrics.sql",
+      ),
+    );
+  }
+
+  async statelessMetricsExplanationSQL() {
+    // Metrics explanations to be displayed in Hover Menu
+    return await spn.TypicalSqlPageNotebook.fetchText(
+      import.meta.resolve(
+        "../metrics-explanation-dml.sql",
+      ),
+    );
+  }
+}
+
+export async function wadwaSQL() {
+  return await spn.TypicalSqlPageNotebook.SQL(
+    new class extends pkg.DRHSqlPages {
+      async statelesswadaSQL() {
+        return await spn.TypicalSqlPageNotebook.fetchText(
+          import.meta.resolve(
+            "../study-specific-stateless/wadwa-stateless.sql",
+          ),
+        );
+      }
+    }(),
+    ...(await pkg.drhNotebooks()),
+    new wadaSqlPages(),
+  );
+}
+
+// this will be used by any callers who want to serve it as a CLI with SDTOUT
+if (import.meta.main) {
+  console.log((await wadwaSQL()).join("\n"));
+}
