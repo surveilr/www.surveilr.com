2fe767ea3d8cc9850b29f466c8910807893729e8
geovlazar <geovlazar@users.noreply.github.com>
2024-12-20 08:54:31 +0000

    deploy: 4187b7f1f5ff2550616ea0d1fc7ed42a5c3a75ea

---
+++ lib/pattern/linkedin-explorer/package.sql
@@ -0,0 +1,8 @@
+Error executing pattern/linkedin-explorer/package.sql: Command failed: lib/pattern/linkedin-explorer/package.sql.ts
+[0m[1m[31merror[0m: Uncaught (in promise) NotFound: Failed to spawn 'surveilr': entity not found
+const { stdout } = await command.output();
+[0m[31m                                 ^[0m
+    at [0m[1m[3mspawnChildInner[0m ([0m[36mext:runtime/40_process.js[0m:[0m[33m184[0m:[0m[33m17[0m)
+    at [0m[1m[3mspawn[0m ([0m[36mext:runtime/40_process.js[0m:[0m[33m388[0m:[0m[33m10[0m)
+    at [0m[1m[3mCommand.output[0m ([0m[36mext:runtime/40_process.js[0m:[0m[33m463[0m:[0m[33m12[0m)
+    at [0m[36mfile:///home/runner/work/www.surveilr.com/www.surveilr.com/lib/std/web-ui-content/shell.ts[0m:[0m[33m6[0m:[0m[33m34[0m
