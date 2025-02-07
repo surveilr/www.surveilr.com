cbb26ed64520dc198264d8823c15c0c67cdbdae9
Baasit <baasisek01@gmail.com>
2025-02-07 10:10:38 +0100

    fix(app): changed view structures to match new model

--- lib/std/web-ui-content/ms.ts
+++ lib/std/web-ui-content/ms.ts
@@ -20,140 +20,135 @@ export class OsqueryMsSqlPages extends spn.TypicalSqlPageNotebook {
             CREATE VIEW surveilr_osquery_ms_node_process AS
             SELECT
                 l.node_key,
-                json_extract(j.value, '$.hostIdentifier') AS host_identifier,
-                json_extract(j.value, '$.calendarTime') AS calendar_time,
-                json_extract(j.value, '$.columns.cgroup_path') AS cgroup_path,
-                json_extract(j.value, '$.columns.cmdline') AS cmdline,
-                json_extract(j.value, '$.columns.cwd') AS cwd,
-                json_extract(j.value, '$.columns.disk_bytes_read') AS disk_bytes_read,
-                json_extract(j.value, '$.columns.disk_bytes_written') AS disk_bytes_written,
-                json_extract(j.value, '$.columns.egid') AS egid,
-                json_extract(j.value, '$.columns.euid') AS euid,
-                json_extract(j.value, '$.columns.gid') AS gid,
-                json_extract(j.value, '$.columns.name') AS process_name,
-                json_extract(j.value, '$.columns.nice') AS nice,
-                json_extract(j.value, '$.columns.on_disk') AS on_disk,
-                json_extract(j.value, '$.columns.parent') AS parent,
-                json_extract(j.value, '$.columns.path') AS process_name,
-                json_extract(j.value, '$.columns.pgroup') AS pgroup,
-                json_extract(j.value, '$.columns.pid') AS pid,
-                json_extract(j.value, '$.columns.resident_size') AS resident_size,
-                json_extract(j.value, '$.columns.root') AS root,
-                json_extract(j.value, '$.columns.sgid') AS sgid,
-                json_extract(j.value, '$.columns.start_time') AS start_time,
-                json_extract(j.value, '$.columns.state') AS state,
-                json_extract(j.value, '$.columns.suid') AS suid,
-                json_extract(j.value, '$.columns.system_time') AS system_time,
-                json_extract(j.value, '$.columns.threads') AS threads,
-                json_extract(j.value, '$.columns.total_size') AS total_size,
-                json_extract(j.value, '$.columns.uid') AS uid,
-                json_extract(j.value, '$.columns.user_time') AS user_time,
-                json_extract(j.value, '$.columns.wired_size') AS wired_size
-            FROM ur_ingest_session_surveilr_osquery_ms_log AS l,
-                json_each(l.log_data) AS j
+                json_extract(l.log_data, '$.hostIdentifier') AS host_identifier,
+                json_extract(l.log_data, '$.calendarTime') AS calendar_time,
+                json_extract(l.log_data, '$.columns.cgroup_path') AS cgroup_path,
+                json_extract(l.log_data, '$.columns.cmdline') AS cmdline,
+                json_extract(l.log_data, '$.columns.cwd') AS cwd,
+                json_extract(l.log_data, '$.columns.disk_bytes_read') AS disk_bytes_read,
+                json_extract(l.log_data, '$.columns.disk_bytes_written') AS disk_bytes_written,
+                json_extract(l.log_data, '$.columns.egid') AS egid,
+                json_extract(l.log_data, '$.columns.euid') AS euid,
+                json_extract(l.log_data, '$.columns.gid') AS gid,
+                json_extract(l.log_data, '$.columns.name') AS process_name,
+                json_extract(l.log_data, '$.columns.nice') AS nice,
+                json_extract(l.log_data, '$.columns.on_disk') AS on_disk,
+                json_extract(l.log_data, '$.columns.parent') AS parent,
+                json_extract(l.log_data, '$.columns.path') AS process_name,
+                json_extract(l.log_data, '$.columns.pgroup') AS pgroup,
+                json_extract(l.log_data, '$.columns.pid') AS pid,
+                json_extract(l.log_data, '$.columns.resident_size') AS resident_size,
+                json_extract(l.log_data, '$.columns.root') AS root,
+                json_extract(l.log_data, '$.columns.sgid') AS sgid,
+                json_extract(l.log_data, '$.columns.start_time') AS start_time,
+                json_extract(l.log_data, '$.columns.state') AS state,
+                json_extract(l.log_data, '$.columns.suid') AS suid,
+                json_extract(l.log_data, '$.columns.system_time') AS system_time,
+                json_extract(l.log_data, '$.columns.threads') AS threads,
+                json_extract(l.log_data, '$.columns.total_size') AS total_size,
+                json_extract(l.log_data, '$.columns.uid') AS uid,
+                json_extract(l.log_data, '$.columns.user_time') AS user_time,
+                json_extract(l.log_data, '$.columns.wired_size') AS wired_size
+            FROM ur_ingest_session_surveilr_osquery_ms_log AS l
             WHERE l.log_type = 'result'
-                AND json_extract(j.value, '$.name') = 'tls_proc';
+                AND json_extract(l.log_data, '$.name') = 'tls_proc';
 
 
         DROP VIEW IF EXISTS surveilr_osquery_ms_node_interface_detail;
         CREATE VIEW surveilr_osquery_ms_node_interface_detail AS
         SELECT
             l.node_key,
-            json_extract(j.value, '$.hostIdentifier') AS host_identifier,
-            json_extract(j.value, '$.calendarTime') AS calendar_time,
-            json_extract(j.value, '$.columns.collisions') AS collisions,
-            json_extract(j.value, '$.columns.flags') AS flags,
-            json_extract(j.value, '$.columns.ibytes') AS ibytes,
-            json_extract(j.value, '$.columns.idrops') AS idrops,
-            json_extract(j.value, '$.columns.ierrors') AS ierrors,
-            json_extract(j.value, '$.columns.interface') AS interface,
-            json_extract(j.value, '$.columns.ipackets') AS ipackets,
-            json_extract(j.value, '$.columns.last_change') AS last_change,
-            json_extract(j.value, '$.columns.link_speed') AS link_speed,
-            json_extract(j.value, '$.columns.mac') AS mac,
-            json_extract(j.value, '$.columns.metric') AS metric,
-            json_extract(j.value, '$.columns.mtu') AS mtu,
-            json_extract(j.value, '$.columns.obytes') AS obytes,
-            json_extract(j.value, '$.columns.odrops') AS odrops,
-            json_extract(j.value, '$.columns.oerrors') AS oerrors,
-            json_extract(j.value, '$.columns.opackets') AS opackets,
-            json_extract(j.value, '$.columns.pci_slot') AS pci_slot,
-            json_extract(j.value, '$.columns.type') AS type
-        FROM ur_ingest_session_surveilr_osquery_ms_log AS l,
-            json_each(l.log_data) AS j
+            json_extract(l.log_data, '$.hostIdentifier') AS host_identifier,
+            json_extract(l.log_data, '$.calendarTime') AS calendar_time,
+            json_extract(l.log_data, '$.columns.collisions') AS collisions,
+            json_extract(l.log_data, '$.columns.flags') AS flags,
+            json_extract(l.log_data, '$.columns.ibytes') AS ibytes,
+            json_extract(l.log_data, '$.columns.idrops') AS idrops,
+            json_extract(l.log_data, '$.columns.ierrors') AS ierrors,
+            json_extract(l.log_data, '$.columns.interface') AS interface,
+            json_extract(l.log_data, '$.columns.ipackets') AS ipackets,
+            json_extract(l.log_data, '$.columns.last_change') AS last_change,
+            json_extract(l.log_data, '$.columns.link_speed') AS link_speed,
+            json_extract(l.log_data, '$.columns.mac') AS mac,
+            json_extract(l.log_data, '$.columns.metric') AS metric,
+            json_extract(l.log_data, '$.columns.mtu') AS mtu,
+            json_extract(l.log_data, '$.columns.obytes') AS obytes,
+            json_extract(l.log_data, '$.columns.odrops') AS odrops,
+            json_extract(l.log_data, '$.columns.oerrors') AS oerrors,
+            json_extract(l.log_data, '$.columns.opackets') AS opackets,
+            json_extract(l.log_data, '$.columns.pci_slot') AS pci_slot,
+            json_extract(l.log_data, '$.columns.type') AS type
+        FROM ur_ingest_session_surveilr_osquery_ms_log AS l
         WHERE l.log_type = 'result'
-        AND json_extract(j.value, '$.name') = 'interface_details';
+        AND json_extract(l.log_data, '$.name') = 'interface_details';
 
         DROP VIEW IF EXISTS surveilr_osquery_ms_node_open_socket;
         CREATE VIEW surveilr_osquery_ms_node_open_socket AS
         SELECT
             l.node_key,
-            json_extract(j.value, '$.hostIdentifier') AS host_identifier,
-            json_extract(j.value, '$.calendarTime') AS calendar_time,
-            json_extract(j.value, '$.columns.family') AS family,
-            json_extract(j.value, '$.columns.fd') AS fd,
-            json_extract(j.value, '$.columns.local_address') AS local_address,
-            json_extract(j.value, '$.columns.local_port') AS local_port,
-            json_extract(j.value, '$.columns.net_namespace') AS net_namespace,
-            json_extract(j.value, '$.columns.path') AS path,
-            json_extract(j.value, '$.columns.pid') AS pid,
-            json_extract(j.value, '$.columns.protocol') AS protocol,
-            json_extract(j.value, '$.columns.remote_address') AS remote_address,
-            json_extract(j.value, '$.columns.remote_port') AS remote_port,
-            json_extract(j.value, '$.columns.socket') AS socket,
-            json_extract(j.value, '$.columns.state') AS state
-        FROM ur_ingest_session_surveilr_osquery_ms_log AS l,
-            json_each(l.log_data) AS j
+            json_extract(l.log_data, '$.hostIdentifier') AS host_identifier,
+            json_extract(l.log_data, '$.calendarTime') AS calendar_time,
+            json_extract(l.log_data, '$.columns.family') AS family,
+            json_extract(l.log_data, '$.columns.fd') AS fd,
+            json_extract(l.log_data, '$.columns.local_address') AS local_address,
+            json_extract(l.log_data, '$.columns.local_port') AS local_port,
+            json_extract(l.log_data, '$.columns.net_namespace') AS net_namespace,
+            json_extract(l.log_data, '$.columns.path') AS path,
+            json_extract(l.log_data, '$.columns.pid') AS pid,
+            json_extract(l.log_data, '$.columns.protocol') AS protocol,
+            json_extract(l.log_data, '$.columns.remote_address') AS remote_address,
+            json_extract(l.log_data, '$.columns.remote_port') AS remote_port,
+            json_extract(l.log_data, '$.columns.socket') AS socket,
+            json_extract(l.log_data, '$.columns.state') AS state
+        FROM ur_ingest_session_surveilr_osquery_ms_log AS l
         WHERE l.log_type = 'result'
-        AND json_extract(j.value, '$.name') = 'process_open_sockets';
+        AND json_extract(l.log_data, '$.name') = 'process_open_sockets';
 
         DROP VIEW IF EXISTS surveilr_osquery_ms_node_system_info;
         CREATE VIEW surveilr_osquery_ms_node_system_info AS
         SELECT
             l.node_key,
-            json_extract(j.value, '$.hostIdentifier') AS host_identifier,
-            json_extract(j.value, '$.calendarTime') AS calendar_time,
-            json_extract(j.value, '$.columns.board_model') AS board_model,
-            json_extract(j.value, '$.columns.board_serial') AS board_serial,
-            json_extract(j.value, '$.columns.board_vendor') AS board_vendor,
-            json_extract(j.value, '$.columns.board_version') AS board_version,
-            json_extract(j.value, '$.columns.computer_name') AS computer_name,
-            json_extract(j.value, '$.columns.cpu_brand') AS cpu_brand,
-            json_extract(j.value, '$.columns.cpu_logical_cores') AS cpu_logical_cores,
-            json_extract(j.value, '$.columns.cpu_microcode') AS cpu_microcode,
-            json_extract(j.value, '$.columns.cpu_physical_cores') AS cpu_physical_cores,
-            json_extract(j.value, '$.columns.cpu_sockets') AS cpu_sockets,
-            json_extract(j.value, '$.columns.cpu_subtype') AS cpu_subtype,
-            json_extract(j.value, '$.columns.cpu_type') AS cpu_type,
-            json_extract(j.value, '$.columns.hardware_model') AS hardware_model,
-            json_extract(j.value, '$.columns.hardware_serial') AS hardware_serial,
-            json_extract(j.value, '$.columns.hardware_vendor') AS hardware_vendor,
-            json_extract(j.value, '$.columns.hardware_version') AS hardware_version,
-            json_extract(j.value, '$.columns.hostname') AS hostname,
-            json_extract(j.value, '$.columns.local_hostname') AS local_hostname,
-            json_extract(j.value, '$.columns.physical_memory') AS physical_memory,
-            json_extract(j.value, '$.columns.uuid') AS uuid
-        FROM ur_ingest_session_surveilr_osquery_ms_log AS l,
-            json_each(l.log_data) AS j
+            json_extract(l.log_data, '$.hostIdentifier') AS host_identifier,
+            json_extract(l.log_data, '$.calendarTime') AS calendar_time,
+            json_extract(l.log_data, '$.columns.board_model') AS board_model,
+            json_extract(l.log_data, '$.columns.board_serial') AS board_serial,
+            json_extract(l.log_data, '$.columns.board_vendor') AS board_vendor,
+            json_extract(l.log_data, '$.columns.board_version') AS board_version,
+            json_extract(l.log_data, '$.columns.computer_name') AS computer_name,
+            json_extract(l.log_data, '$.columns.cpu_brand') AS cpu_brand,
+            json_extract(l.log_data, '$.columns.cpu_logical_cores') AS cpu_logical_cores,
+            json_extract(l.log_data, '$.columns.cpu_microcode') AS cpu_microcode,
+            json_extract(l.log_data, '$.columns.cpu_physical_cores') AS cpu_physical_cores,
+            json_extract(l.log_data, '$.columns.cpu_sockets') AS cpu_sockets,
+            json_extract(l.log_data, '$.columns.cpu_subtype') AS cpu_subtype,
+            json_extract(l.log_data, '$.columns.cpu_type') AS cpu_type,
+            json_extract(l.log_data, '$.columns.hardware_model') AS hardware_model,
+            json_extract(l.log_data, '$.columns.hardware_serial') AS hardware_serial,
+            json_extract(l.log_data, '$.columns.hardware_vendor') AS hardware_vendor,
+            json_extract(l.log_data, '$.columns.hardware_version') AS hardware_version,
+            json_extract(l.log_data, '$.columns.hostname') AS hostname,
+            json_extract(l.log_data, '$.columns.local_hostname') AS local_hostname,
+            json_extract(l.log_data, '$.columns.physical_memory') AS physical_memory,
+            json_extract(l.log_data, '$.columns.uuid') AS uuid
+        FROM ur_ingest_session_surveilr_osquery_ms_log AS l
         WHERE l.log_type = 'result'
-        AND json_extract(j.value, '$.name') = 'system_info';
+        AND json_extract(l.log_data, '$.name') = 'system_info';
 
         DROP VIEW IF EXISTS surveilr_osquery_ms_node_interface_address;
         CREATE VIEW surveilr_osquery_ms_node_interface_address AS
         SELECT
             l.node_key,
-            json_extract(j.value, '$.hostIdentifier') AS host_identifier,
-            json_extract(j.value, '$.calendarTime') AS calendar_time,
-            json_extract(j.value, '$.columns.address') AS address,
-            json_extract(j.value, '$.columns.broadcast') AS broadcast,
-            json_extract(j.value, '$.columns.interface') AS interface,
-            json_extract(j.value, '$.columns.mask') AS mask,
-            json_extract(j.value, '$.columns.point_to_point') AS point_to_point,
-            json_extract(j.value, '$.columns.type') AS type
-        FROM ur_ingest_session_surveilr_osquery_ms_log AS l,
-            json_each(l.log_data) AS j
+            json_extract(l.log_data, '$.hostIdentifier') AS host_identifier,
+            json_extract(l.log_data, '$.calendarTime') AS calendar_time,
+            json_extract(l.log_data, '$.columns.address') AS address,
+            json_extract(l.log_data, '$.columns.broadcast') AS broadcast,
+            json_extract(l.log_data, '$.columns.interface') AS interface,
+            json_extract(l.log_data, '$.columns.mask') AS mask,
+            json_extract(l.log_data, '$.columns.point_to_point') AS point_to_point,
+            json_extract(l.log_data, '$.columns.type') AS type
+        FROM ur_ingest_session_surveilr_osquery_ms_log AS l
         WHERE l.log_type = 'result'
-        AND json_extract(j.value, '$.name') = 'interface_addresses';
+        AND json_extract(l.log_data, '$.name') = 'interface_addresses';
 
         DROP VIEW IF EXISTS surveilr_osquery_ms_node_detail;
         CREATE VIEW surveilr_osquery_ms_node_detail AS
@@ -214,7 +209,7 @@ export class OsqueryMsSqlPages extends spn.TypicalSqlPageNotebook {
             ${this.activeBreadcrumbsSQL({ titleExpr: `$host_id || ' Node'` })}
             
             SELECT 'datagrid' as component;
-            SELECT 'Computer Name' as title, "computer_name" as description FROM surveilr_osquery_ms_node_system_info WHERE node_key = $key LIMIT 1;
+            SELECT 'Computer Name' as title, "computer_name" as description FROM surveilr_osquery_ms_node_system_info;
             SELECT 'Cpu Brand' as title, "cpu_brand" as description FROM surveilr_osquery_ms_node_system_info WHERE node_key = $key LIMIT 1;
             SELECT 'cpu_type' as title, "cpu_type" as description FROM surveilr_osquery_ms_node_system_info WHERE node_key = $key LIMIT 1;
             SELECT 'cpu_logical_cores' as title, "cpu_logical_cores" as description FROM surveilr_osquery_ms_node_system_info WHERE node_key = $key LIMIT 1;
