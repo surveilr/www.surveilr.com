a793c00553f0908b19dc921b6d20a794522f53a4
MeetAnithaVarghese <MeetAnithaVarghese@users.noreply.github.com>
2025-02-14 05:19:48 +0000

    deploy: 0399be0d2aab2b3cb33a74d8975f3911a93be1bc

--- lib/service/diabetes-research-hub/study-specific-stateless/generate-cgm-combined-sql.ts
+++ lib/service/diabetes-research-hub/study-specific-stateless/generate-cgm-combined-sql.ts
@@ -594,6 +594,7 @@ export function saveCTRJsonCgm(dbFilePath: string): string {
   const rows = db.prepare(`SELECT * FROM ${tableName}`).all();
 
   db.exec(`CREATE TABLE IF NOT EXISTS file_meta_ingest_data (
+    file_meta_id text not null,
     db_file_id TEXT NOT NULL,
     participant_display_id TEXT NOT NULL,
     file_meta_data TEXT NULL,
@@ -640,12 +641,14 @@ export function saveCTRJsonCgm(dbFilePath: string): string {
       deidentID,
     );
 
+    const file_meta_id = ulid();
+
     db.prepare(
-      `INSERT INTO file_meta_ingest_data(db_file_id, participant_display_id, cgm_data, file_meta_data) VALUES (?, ?, ?, ?);`,
-    ).run(db_file_id, row.patient_id, jsonStringCgm, jsonStringMeta);
+      `INSERT INTO file_meta_ingest_data(file_meta_id, db_file_id, participant_display_id, cgm_data, file_meta_data) VALUES (?, ?, ?, ?,?);`,
+    ).run(file_meta_id, db_file_id , row.patient_id, jsonStringCgm, jsonStringMeta);
   }
 
-  db.close();
+  db.close(); 
   return ctrSQL;
 }
 
