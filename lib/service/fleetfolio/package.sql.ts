dfece9f948c3e9c6f7a60bd1b5b8fa99de5dc0a3
Joby James <joby.james@citrusinformatics.in>
2025-03-03 12:33:57 +0000

    feat: add all process listing #355

--- lib/service/fleetfolio/package.sql.ts
+++ lib/service/fleetfolio/package.sql.ts
@@ -9,9 +9,9 @@ import {
 } from "../../std/web-ui-content/mod.ts";
 import * as sh from "./custom_shell.ts";
 
-const WEB_UI_TITLE = "Feetfolio";
-const WE_UI_LOGO = "feetfolio-logo.png";
-const WE_UI_FAV_ICON = "feetfolio-favicon.ico";
+const WEB_UI_TITLE = "fleetfolio";
+const WE_UI_LOGO = "fleetfolio-logo.png";
+const WE_UI_FAV_ICON = "fleetfolio-favicon.ico";
 
 /**
  * These pages depend on ../../std/package.sql.ts being loaded into RSSD (for nav).
@@ -26,10 +26,10 @@ const WE_UI_FAV_ICON = "feetfolio-favicon.ico";
  */
 
 // custom decorator that makes navigation for this notebook type-safe
-function qltyfolioNav(route: Omit<spn.RouteConfig, "path" | "parentPath">) {
+function fleetfolioNav(route: Omit<spn.RouteConfig, "path" | "parentPath">) {
   return spn.navigationPrime({
     ...route,
-    parentPath: "qltyfolio/index.sql",
+    parentPath: "fleetfolio/index.sql",
   });
 }
 
@@ -41,8 +41,8 @@ export class FleetFolioSqlPages extends spn.TypicalSqlPageNotebook {
   // or `DDL` as general SQL before doing any upserts into sqlpage_files.
   navigationDML() {
     return this.SQL`
-      -- delete all /qltyfolio-related entries and recreate them in case routes are changed
-      DELETE FROM sqlpage_aide_navigation WHERE parent_path like ${this.constructHomePath("qltyfolio")
+      -- delete all /fleetfolio-related entries and recreate them in case routes are changed
+      DELETE FROM sqlpage_aide_navigation WHERE parent_path like ${this.constructHomePath("fleetfolio")
       };
       ${this.upsertNavSQL(...Array.from(this.navigation.values()))}
     `;
@@ -52,7 +52,7 @@ export class FleetFolioSqlPages extends spn.TypicalSqlPageNotebook {
     caption: "FleetFolio",
     description: `FleetFolio is a powerful infrastructure assurance platform built on surveilr that helps organizations achieve continuous compliance, security, and operational reliability. Unlike traditional asset management tools that simply list discovered assets, FleetFolio takes a proactive approach by defining expected infrastructure assets and verifying them against actual assets found using osQuery Management Server (MS).`,
   })
-  "cak/index.sql"() {
+  "fleetfolio/index.sql"() {
     return this.SQL`
     select
         'text'              as component,
@@ -60,29 +60,233 @@ export class FleetFolioSqlPages extends spn.TypicalSqlPageNotebook {
       WITH navigation_cte AS (
           SELECT COALESCE(title, caption) as title, description
             FROM sqlpage_aide_navigation
-           WHERE namespace = 'prime' AND path = ${this.constructHomePath("cak")}
+           WHERE namespace = 'prime' AND path = ${this.constructHomePath("fleetfolio")}
       )
       SELECT 'list' AS component, title, description
         FROM navigation_cte;
       SELECT caption as title, ${this.absoluteURL("/")
       } || COALESCE(url, path) as link, description
         FROM sqlpage_aide_navigation
-       WHERE namespace = 'prime' AND parent_path = ${this.constructHomePath("cak")
+       WHERE namespace = 'prime' AND parent_path = ${this.constructHomePath("fleetfolio")
       }
        ORDER BY sibling_order;`;
   }
+
+  @fleetfolioNav({
+    caption: "Server",
+    description:
+      `The Server (Host) List ingested via osQuery provides real-time visibility into all discovered infrastructure assets.`,
+    siblingOrder: 1,
+  })
+  "fleetfolio/host.sql"() {
+    return this.SQL`
+        ${this.activePageTitle()}
+  
+        -- sets up $limit, $offset, and other variables (use pagination.debugVars() to see values in web-ui)
+          --- Dsply Page Title
+      SELECT
+          'title'   as component,
+          'Server ' contents;
+  
+         select
+          'text'              as component,
+          'The Server (Host) List ingested via osQuery provides real-time visibility into all discovered infrastructure assets.' as contents;
+  
+        -- Dashboard count
+        select
+            'card' as component,
+            4      as columns;
+        select
+            host_identifier  as title,
+            ${this.absoluteURL('host_detail.sql?link=')} || host_identifier as link
+        FROM surveilr_osquery_ms_node_detail;
+        `;
+  }
+
+  @spn.shell({ breadcrumbsFromNavStmts: "no" })
+  "fleetfolio/host_detail.sql"() {
+    return this.SQL`
+
+    --- Display breadcrumb
+     SELECT
+        'breadcrumb' AS component;
+      SELECT
+        'Home' AS title,
+        ${this.absoluteURL("/")}    AS link;
+      SELECT
+        'Server' AS title,
+        ${this.absoluteURL("/fleetfolio/index.sql")} AS link;
+      SELECT
+        host_identifier as title
+      FROM
+        surveilr_osquery_ms_node_detail 
+      WHERE
+       host_identifier = $link::TEXT;
+
+       --- Dsply Page Title
+      SELECT
+       'title' as component,
+        host_identifier as contents
+      FROM
+        surveilr_osquery_ms_node_detail
+      WHERE
+        host_identifier = $link::TEXT;
+
+     select 
+    'datagrid' as component;
+    select 
+          'Host identifier' as title,
+          host_identifier   as description FROM surveilr_osquery_ms_node_system_info WHERE host_identifier = $link::TEXT;
+    select 
+          'Model' as title,
+          board_model   as description FROM surveilr_osquery_ms_node_system_info WHERE host_identifier = $link::TEXT;  
+    select 
+          'Serial No' as title,
+          board_serial   as description FROM surveilr_osquery_ms_node_system_info WHERE host_identifier = $link::TEXT;  
+    select 
+          'Vendor' as title,
+          board_vendor   as description FROM surveilr_osquery_ms_node_system_info WHERE host_identifier = $link::TEXT;  
+    select 
+          'Version' as title,
+          board_version   as description FROM surveilr_osquery_ms_node_system_info WHERE host_identifier = $link::TEXT; 
+    select 
+          'CPU brand' as title,
+          cpu_brand   as description FROM surveilr_osquery_ms_node_system_info WHERE host_identifier = $link::TEXT;  
+    select 
+          'CPU brand' as title,
+          cpu_brand   as description FROM surveilr_osquery_ms_node_system_info WHERE host_identifier = $link::TEXT; 
+
+     select
+          'card' as component,
+          4      as columns;
+      SELECT name  as title,
+      count as description_md,
+      CASE
+        WHEN name = 'All Processes'
+        THEN ${this.absoluteURL('all_process.sql?host_identifier=')} || hostIdentifier || '&name=' || name 
+        ELSE NULL 
+      END AS link
+      FROM system_detail_group WHERE hostIdentifier = $link::TEXT
+          `;
+  }
+
+  @spn.shell({ breadcrumbsFromNavStmts: "no" })
+  "fleetfolio/all_process.sql"() {
+    const viewName = `system_detail_all_processes`;
+    const pagination = this.pagination({
+      tableOrViewName: viewName,
+      whereSQL: "WHERE host_identifier=$host_identifier",
+    });
+    return this.SQL`
+
+    --- Display breadcrumb
+     SELECT
+        'breadcrumb' AS component;
+      SELECT
+        'Home' AS title,
+        ${this.absoluteURL("/")}    AS link;
+      SELECT
+        'Server' AS title,
+        ${this.absoluteURL("/fleetfolio/index.sql")} AS link;
+        
+      SELECT
+        $host_identifier AS title,
+        ${this.absoluteURL("/fleetfolio/host_detail.sql?link=")} || $host_identifier AS link;
+      
+      SELECT
+        "Detail" AS title,
+        '#' AS link;
+      
+
+       --- Dsply Page Title
+      SELECT
+       'title' as component,
+        host_identifier as contents
+      FROM
+        surveilr_osquery_ms_node_detail
+      WHERE
+        host_identifier = $host_identifier::TEXT;
+       ${pagination.init()}
+      select 
+          'table'           as component,
+          TRUE              as sort,
+          'hostIdentifier',
+          'cgroup_path',
+          'cmdline',
+          'cwd',
+          'disk_bytes_read',
+          'disk_bytes_written',
+          'egid',
+          'euid',
+          'gid',
+          'system_name',
+          'nice',
+          'on_disk',
+          'parent',
+          'path',
+          'pgroup',
+          'pid',
+          'resident_size',
+          'root',
+          'sgid',
+          'start_time',
+          'state',
+          'suid',
+          'system_time',
+          'threads',
+          'total_size',
+          'uid',
+          'user_time',
+          'wired_size',
+          'updated_at';
+      SELECT 
+        host_identifier,
+        cgroup_path,
+        cmdline,
+        cwd,
+        disk_bytes_read,
+        disk_bytes_written,
+        egid,
+        euid,
+        gid,
+        system_name,
+        nice,
+        on_disk,
+        parent,
+        path,
+        pgroup,
+        pid,
+        resident_size,
+        root,
+        sgid,
+        start_time,
+        state,
+        suid,
+        system_time,
+        threads,
+        total_size,
+        uid,
+        user_time,
+        wired_size,
+        updated_at FROM ${viewName} WHERE host_identifier = $host_identifier::TEXT LIMIT $limit
+      OFFSET $offset;
+      ${pagination.renderSimpleMarkdown('host_identifier')};`;
+
+
+  }
+
 }
 
 export async function SQL() {
   return await spn.TypicalSqlPageNotebook.SQL(
     new class extends spn.TypicalSqlPageNotebook {
-      async statefulqltyfolioSQL() {
+      async statefulfleetfolioSQL() {
         // read the file from either local or remote (depending on location of this file)
         return await spn.TypicalSqlPageNotebook.fetchText(
           import.meta.resolve("./stateful.sql"),
         );
       }
-      async statelessqltyfolioSQL() {
+      async statelessfleetfolioSQL() {
         // read the file from either local or remote (depending on location of this file)
         return await spn.TypicalSqlPageNotebook.fetchText(
           import.meta.resolve("./stateless.sql"),
