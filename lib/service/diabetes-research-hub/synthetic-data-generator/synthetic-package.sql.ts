84f8d685d1b2833e61601ad253884e4b132b6ef8
sijucj <sijucj@users.noreply.github.com>
2025-03-11 11:51:18 +0000

    deploy: 066d340f2fdaacd65b18b3bf0c85792dda531709

---
+++ lib/service/diabetes-research-hub/synthetic-data-generator/synthetic-package.sql.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --allow-env --allow-run --allow-sys --allow-ffi
+import { sqlPageNB as spn } from "../deps.ts";
+import * as pkg from "../drh-basepackage.sql.ts";
+import { Database } from "https://deno.land/x/sqlite3@0.12.0/mod.ts";
+import { ulid } from "https://deno.land/x/ulid/mod.ts";
+
+
+export class syntheticSqlPages extends spn.TypicalSqlPageNotebook {  
+  //metrics static views shall be generated after the combined_cgm_tracing is created.
+  async statelessMetricsSQL() {
+    // stateless SQL for the metrics
+    return await spn.TypicalSqlPageNotebook.fetchText(
+      import.meta.resolve(
+        "../drh-metrics.sql",
+      ),
+    );
+  }
+
+  async statelessMetricsExplanationSQL() {
+    // Metrics explanations to be displayed in Hover Menu
+    return await spn.TypicalSqlPageNotebook.fetchText(
+      import.meta.resolve(
+        "../metrics-explanation-dml.sql",
+      ),
+    );
+  }
+}
+
+export async function syntheticSQL() {
+  return await spn.TypicalSqlPageNotebook.SQL(
+    new class extends pkg.DRHSqlPages {
+      async statelessSyntheticSQL() {
+        return await spn.TypicalSqlPageNotebook.fetchText(
+          import.meta.resolve(
+            "../study-specific-stateless/synthetic-stateless.sql",
+          ),
+        );
+      }
+    }(),
+    ...(await pkg.drhNotebooks()),
+    new syntheticSqlPages(),
+  );
+}
+
+// this will be used by any callers who want to serve it as a CLI with SDTOUT
+if (import.meta.main) {
+  console.log((await syntheticSQL()).join("\n"));
+}
