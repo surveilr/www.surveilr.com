dc66e20ba9bfcc586e745effc6d94ceb85526c38
geovlazar <geovlazar@users.noreply.github.com>
2025-08-11 04:34:56 +0000

    deploy: 34f30504e63d421deedd987504375438816ce92a

--- lib/pattern/privacy/anonymize-sample/ux.sql
+++ lib/pattern/privacy/anonymize-sample/ux.sql
@@ -1,58 +1,172 @@
-Error executing pattern/privacy/anonymize-sample/ux.sql: Command failed: deno run -A lib/pattern/privacy/anonymize-sample/ux.sql.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/sqlpage/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/domain/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/dml/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/dql/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/graph.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/domain/domain.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/domain/domains.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/dml/insert.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/schema.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/type.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/view.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/dql/criteria.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/dql/select.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/dialect.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/directive.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/js.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/interpolate.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/quality-system.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/namespace.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/sql-notebook.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/sql.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/polygenix-notebook.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/emit/polygenix.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/deps.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/universal/safety.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/quality-system/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/quality-system.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/universal/zod-aide.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/universal/record.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/column.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/constraint.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/foreign-key.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/index.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/lint.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/primary-key.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/record.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/select.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/self-ref.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/render/ddl/table/table.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/universal/whitespace.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/pre-process/psql/mod.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/notebook/chain-of-responsibility.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/notebook/command.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/deps.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/quality-system/governance.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/pre-process/psql/meta-command.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/pre-process/psql/set.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/pre-process/psql/include.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/universal/graph.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/notebook/deps.ts
-[0m[32mDownload[0m https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/universal/flexible-text.ts
-[0m[1m[31merror[0m: Module not found "https://raw.githubusercontent.com/netspective-labs/sql-aide/v0.13.40/lib/sqlpage/mod.ts".
-    at [0m[36mfile:///home/runner/work/www.surveilr.com/www.surveilr.com/lib/pattern/privacy/anonymize-sample/deps.ts[0m:[0m[33m2[0m:[0m[33m30[0m
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/index.sql',
+      'SELECT
+  ''list'' as component,
+  ''DATA CONVERSION'' as title,
+  ''Here are some useful links.'' as description;        
+SELECT ''Device Summary'' as title,
+''../health_device_summary.sql'' as link,
+''Provides information about the device from which the data conversion is carried out'' as description;            
+SELECT ''Files List'' as title,
+  ''health_converted_file_list.sql'' as link,
+  ''Provides the files being converted'' as description;
+SELECT ''Tables'' as title,
+  ''health_table_list.sql'' as link,
+  ''List of the tables'' as description;        
+',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health_device_summary.sql',
+      'SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from device_data;      
+select 
+''text'' as component,
+''[Back to Home](health/index.sql)'' as contents_md;',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/health_converted_file_list.sql',
+      '--select ''Number of Files converted'' as Title,select * from number_of_files_converted as description;
+SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from converted_files_list;
+select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/health_table_list.sql',
+      'SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from converted_table_list;
+--select ''table'' as component, ''action'' as markdown;
+  --select *,
+  --format(''[View](view_table_snapshot.sql?id=%s)'', table_name) as action
+  --from converted_table_list
+  select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;;
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/view_table_snapshot.sql',
+      '
+select 
+''title''   as component,
+''Table Data Snapshot'' as contents,
+ 1    as level;       
+select 
+''text'' as component,
+''Study Table'' as contents_md;  
+  SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+  select *        
+  from study_data ; 
+  select 
+''text'' as component,
+''CGM File Meta Data table'' as contents_md;  
+  SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+  select *        
+  from cgmfilemetadata_view ; 
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/study_data.sql',
+      '
+  SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+  SELECT * from study_data;
+  select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+    ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/cgmfilemetadata.sql',
+      '
+    SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+    SELECT * from cgmfilemetadata_view;
+    select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+      ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/author_data.sql',
+      '
+SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from author_data;
+select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/institution_data.sql',
+      '
+SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from institution_data;
+select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/investigator_data.sql',
+      '
+SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from investigator_data;
+select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/lab_data.sql',
+      '
+SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from lab_data;
+select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/participant_data.sql',
+      '
+SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from participant_data;
+select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/publication_data.sql',
+      '
+SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from publication_data;
+select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
+INSERT INTO sqlpage_files (path, contents, last_modified) VALUES (
+      'health/site_data.sql',
+      '
+SELECT ''table'' as component, 1 as search, 1 as sort, 1 as hover, 1 as striped_rows;
+SELECT * from site_data;
+select 
+''text'' as component,
+''[Back to Home](index.sql)'' as contents_md;
+  ',
+      CURRENT_TIMESTAMP)
+  ON CONFLICT(path) DO UPDATE SET contents = EXCLUDED.contents, last_modified = CURRENT_TIMESTAMP;
