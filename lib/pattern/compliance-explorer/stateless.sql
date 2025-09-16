28ff99b5e661539e52a910933a2fa103a31cab74
Divya <divya.ts@citrusinformatics.com>
2025-09-16 12:37:37 +0530

    chore: add  compliance regime controls and SQL Page listings #355

--- lib/pattern/compliance-explorer/stateless.sql
+++ lib/pattern/compliance-explorer/stateless.sql
@@ -68,3 +68,64 @@ SELECT
   tenant_name
 FROM uniform_resource_aicpa_soc2_controls
 WHERE "Control Identifier" IS NOT NULL AND "Control Identifier" != '';
+
+
+DROP VIEW IF EXISTS compliance_regime_control_hitrust_e1;
+
+CREATE VIEW compliance_regime_control_hitrust_e1 AS
+SELECT
+  "#" AS control_code,
+  "Control Identifier" AS control_id,
+  "Fii ID" AS fii_id,
+  "Common Criteria" AS common_criteria,
+  NULL AS criteria_type, -- not available in this table
+  Name AS control_name,
+  Description AS control_question,
+  'HITRUST E1' AS control_type,
+  tenant_id,
+  tenant_name
+FROM uniform_resource_hitrust_e1_assessment
+WHERE "Control Identifier" IS NOT NULL 
+  AND "Control Identifier" != '';
+
+DROP VIEW IF EXISTS compliance_iso_27001_control;
+
+CREATE VIEW compliance_iso_27001_control AS
+SELECT 
+    `SCF Domain` AS scf_domain,
+    `SCF Control` AS scf_control,
+    `SCF #` AS control_code,
+    `Secure Controls Framework (SCF)
+Control Description` AS control_description,
+    `SCF Control Question` AS control_question,
+    Evidence AS evidence,
+    tenant_id,
+    tenant_name,
+    'ISO 27001 v3' AS control_type
+FROM uniform_resource_iso_27001_v3;
+
+DROP VIEW IF EXISTS hipaa_security_rule_safeguards;
+CREATE VIEW hipaa_security_rule_safeguards AS
+SELECT
+    "#" AS id,
+    "Common Criteria" AS common_criteria,
+    "HIPAA Security Rule Reference" AS hipaa_security_rule_reference,
+    Safeguard AS safeguard,
+    "Handled by nQ" AS handled_by_nq,
+    "FII Id" AS fii_id,
+    tenant_id,
+    tenant_name
+FROM uniform_resource_hipaa_security_rule_safeguards;
+ 
+DROP VIEW IF EXISTS compliance_regime_thsa;
+CREATE VIEW compliance_regime_thsa AS
+SELECT
+   "#" AS id,
+  `SCF Domain` AS scf_domain,
+  `SCF Control` AS scf_control,
+  `SCF Control Question` AS scf_control_question,
+  "SCF #" AS scf_code,
+  "Your Answer" AS your_answer,
+  tenant_id,
+  tenant_name
+FROM uniform_resource_thsa;
