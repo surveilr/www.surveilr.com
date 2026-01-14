dfdc5702cef6a195c2d91d2db0b13bc6c971c383
Lilit0x <Lilit0x@users.noreply.github.com>
2026-01-14 18:41:00 +0000

    deploy: 588717146387cd22516bc5bcdf16517f58cbfd44

--- lib/std/drizzle/bootstrap.sql
+++ lib/std/drizzle/bootstrap.sql
@@ -1,3 +1,5 @@
 Error executing std/drizzle/bootstrap.sql: Command failed: deno run -A lib/std/drizzle/bootstrap.sql.ts
-[0m[1m[31merror[0m: Relative import path "drizzle-orm" not prefixed with / or ./ or ../ and not in import map from "file:///home/runner/work/www.surveilr.com/www.surveilr.com/lib/std/drizzle/models.ts"
-    at [0m[36mfile:///home/runner/work/www.surveilr.com/www.surveilr.com/lib/std/drizzle/models.ts[0m:[0m[33m17[0m:[0m[33m21[0m
+[0m[1m[31merror[0m: Failed reading lockfile at '/home/runner/work/www.surveilr.com/www.surveilr.com/deno.lock'
+
+Caused by:
+    Unsupported lockfile version '5'. Try upgrading Deno or recreating the lockfile
